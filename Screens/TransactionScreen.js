fdab3ab15de72e0a406e255f22ebb40eb565b164
Manasa <Manasa9955j@gmail.com>
2020-11-09 17:18:49 +0530

    c69

--- Screens/TransactionScreen.js
+++ Screens/TransactionScreen.js
@@ -1,12 +1,53 @@
 import * as React from "react";
-import {View ,Text} from "react-native";
+import {View ,Text,TouchableOpacity, StyleSheet} from "react-native";
+import * as Permissions from "expo-permissions";
+import {BarCodeScanner} from "expo-barcode-scanner";
 
 export default class TransactionScreen extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            hasCameraPermission:null,
+            scanned:false,
+            scanData:"",
+            buttonState:"normal"
+        }
+
+    }
+    getCameraPermission=async()=>{
+        const {status}= await Permissions.askAsync(Permissions.CAMERA)
+        this.setState({
+            hasCameraPermission:status==="granted",
+            buttonState:"clicked"
+        })
+    }
+    handleBareCodeScanner=async({type,data})=>{
+        this.setState({
+            scanned:true,
+            scanData:data,
+            buttonState:"normal"
+        })
+    }
     render(){
-        return(
-        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-         <Text>TransactionScreen</Text>   
-        </View>
-        )
+        const hasCameraPermission=this.state.hasCameraPermission;
+        const scanned=this.state.scanned;
+        const buttonState=this.state.buttonState;
+
+        if(buttonState==="clicked" && hasCameraPermission===true){
+           return(
+                <BarCodeScanner 
+                onBarCodeScanned={scanned?undefined:this.handleBareCodeScanner}
+                style={StyleSheet.absoluteFillObject}
+                ></BarCodeScanner>
+           )
+        }else if (buttonState==="normal"){
+            return(
+                <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+                 <Text>{hasCameraPermission===true?this.state.scanData:"requestCameraPermission"}</Text>
+                 <TouchableOpacity onPress={this.getCameraPermission}><Text>Scan QR Code</Text></TouchableOpacity>   
+                </View>
+                )
+        }
+       
     }
 }
